ba52c7a4bff994aada72958609f1aa7388900303
Steve Cookson <steve.cookson@sca-uk.com>
2024-06-26 20:18:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-#pip3 install flask flask-cors smbus2 RPi.bme280 adafruit-blinka adafruit-circuitpython-scd30 pandas requests psutil
+# Clear supervisor configuration files
+sudo rm /etc/supervisor/conf.d/*.conf
+# Copy new Supervisor configuration files
+sudo cp *.conf /etc/supervisor/conf.d/
+# Install required Python packages within the virtual environment
+pip3 install flask flask-cors smbus2 RPi.bme280 adafruit-blinka adafruit-circuitpython-scd30 pandas requests psutil
