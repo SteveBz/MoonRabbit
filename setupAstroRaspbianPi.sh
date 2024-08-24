01ff90632d2226886a535f362b1ea667e238e5b3
Steve Cookson <steve.cookson@sca-uk.com>
2024-08-24 07:16:25 +0100

    Update setupAstroRaspbianPi.sh

--- setupAstroRaspbianPi.sh
+++ setupAstroRaspbianPi.sh
@@ -1041,7 +1041,7 @@ EOF
 
 #########################################################
 
-
+sudo apt-get install swig
 pip install pyindi-client
 
 # This will make the utility scripts in the folder executable in case the user wants to use them.
