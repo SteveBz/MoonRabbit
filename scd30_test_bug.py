725f702367354f3b0ab2faa6d8170d643a017243
Steve Cookson <steve.cookson@sca-uk.com>
2025-04-05 15:02:30 +0100

    Update scd30_test_bug.py

--- scd30_test_bug.py
+++ scd30_test_bug.py
@@ -25,7 +25,7 @@ ambient_pressure_hpa = int(pressure_val)
 
 # SCD-30 has tempremental I2C with clock stretching, datasheet recommends
 # starting at 50KHz
-i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
+i2c = busio.I2C(board.SCL, board.SDA) # , frequency=50000
 #scd = adafruit_scd30.SCD30(i2c, ambient_pressure = int(ambient_pressure_hpa))
 scd = adafruit_scd30.SCD30(i2c)
 print("Warming up the SCD30 sensor...")
