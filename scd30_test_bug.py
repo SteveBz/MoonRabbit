d1f0d82cf6a931ce0b5d13cb8b53cb8c945dc49a
Steve Cookson <steve.cookson@sca-uk.com>
2025-04-05 17:53:24 +0100

    Update scd30_test_bug.py

--- scd30_test_bug.py
+++ scd30_test_bug.py
@@ -49,30 +49,54 @@ print("Altitude:", scd.altitude, "meters above sea level")
 #scd.forced_recalibration_reference = 409
 print("Forced recalibration reference:", scd.forced_recalibration_reference)
 print("")
-
+def read_sensors():
+    try:
+        sample_reading = bme280.sample(bus, address, calibration_params)
+        temperature_val = sample_reading.temperature
+        ambient_pressure_hpa = int(sample_reading.pressure)
+        
+        # Update ambient pressure *without* reinitializing the sensor
+        scd.ambient_pressure = ambient_pressure_hpa
+        scd.temperature_offset = scd.temperature - temperature_val
+        
+        time.sleep(2)
+        data = scd.data_available
+        if data:
+            print("Data Available.")
+            print(f"CO2: {scd.CO2:.1f} ppm")
+            print(f"Temperature: {scd.temperature:.1f} °C")
+            print(f"Temperature offset: {(scd.temperature - temperature_val):.1f} °C")
+            print(f"Humidity: {scd.relative_humidity:.1f} %RH")
+            print(f"Ambient Pressure (BME280): {ambient_pressure_hpa} hPa")
+            print("")
+            print("Waiting for new data...")
+    
+            print("")
+    except Exception as e:
+        print(f"Sensor read error: {e}")
 while True:
     
-    sample_reading = bme280.sample(bus, address, calibration_params)
-    temperature_val = sample_reading.temperature
-    humidity_val = sample_reading.humidity
+    #sample_reading = bme280.sample(bus, address, calibration_params)
+    #temperature_val = sample_reading.temperature
+    #humidity_val = sample_reading.humidity
     #pressure_val = sample_reading.pressure
-    ambient_pressure_hpa = int(sample_reading.pressure)
+    #ambient_pressure_hpa = int(sample_reading.pressure)
     
     # Update ambient pressure *without* reinitializing the sensor
-    scd.ambient_pressure = ambient_pressure_hpa
-    scd.temperature_offset = scd.temperature - temperature_val
+    #scd.ambient_pressure = ambient_pressure_hpa
+    #scd.temperature_offset = scd.temperature - temperature_val
     
-    time.sleep(2)
-    data = scd.data_available
-    if data:
-        print("Data Available!")
-        print(f"CO2: {scd.CO2:.1f} ppm")
-        print(f"Temperature: {scd.temperature:.1f} °C")
-        print(f"Temperature offset: {(scd.temperature - temperature_val):.1f} °C")
-        print(f"Humidity: {scd.relative_humidity:.1f} %RH")
-        print(f"Ambient Pressure (BME280): {ambient_pressure_hpa} hPa")
-        print("")
-        print("Waiting for new data...")
-        print("")
-
+    #time.sleep(2)
+    #data = scd.data_available
+    #if data:
+    #    print("Data Available!")
+    #    print(f"CO2: {scd.CO2:.1f} ppm")
+    #    print(f"Temperature: {scd.temperature:.1f} °C")
+    #    print(f"Temperature offset: {(scd.temperature - temperature_val):.1f} °C")
+    #    print(f"Humidity: {scd.relative_humidity:.1f} %RH")
+    #    print(f"Ambient Pressure (BME280): {ambient_pressure_hpa} hPa")
+    #    print("")
+    #    print("Waiting for new data...")
+    #    print("")
+    read_sensors()
     time.sleep(2)
