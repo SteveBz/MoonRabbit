b882920eb01d79ba1911103f4da80dffdbffe345
Steve Cookson <steve.cookson@sca-uk.com>
2025-05-13 14:55:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ sudo rm /etc/supervisor/conf.d/*.conf
 # Copy new Supervisor configuration files
 sudo cp *.conf /etc/supervisor/conf.d/
 # Install required Python packages within the virtual environment
-pip3 install flask flask-cors smbus2 RPi.bme280 adafruit-blinka adafruit-circuitpython-scd30 pandas requests psutil
+pip3 install flask flask-cors smbus2 RPi.bme280 adafruit-blinka==8.40.0 adafruit-circuitpython-scd30 pandas requests psutil
 
 sudo apt-get -y autoremove
 sudo reboot
