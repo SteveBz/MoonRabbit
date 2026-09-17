d60a07c2a91542b9a1202c1651570085d1958892
Steve Cookson <steve.cookson@sca-uk.com>
2026-09-17 13:06:13 +0100

    Refactor CO₂ trace line properties in dashboard.js - REVERT

--- static/dashboard.js
+++ static/dashboard.js
@@ -264,10 +264,10 @@ function loadGlobalCO2() {
             x: [],
             y: [],
             name: 'CO₂',
-            mode: 'lines+markers',
+            mode: 'lines',
             type: 'scatter',
             yaxis: 'y2',
-            line: { color: '#008080', width: 2 },
+            line: { color: '#008080', width: 1.5 },
             opacity: 0.5
         };
 
