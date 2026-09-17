34dc3f36df9aab5b8ccfa79cbcd827dcf7127639
Steve Cookson <steve.cookson@sca-uk.com>
2026-09-17 11:03:14 +0100

    Change CO2 color code in SENSOR_METADATA

--- app_v2.py
+++ app_v2.py
@@ -22,7 +22,7 @@ SENSOR_METADATA = {
     "temperature": {"unit": "°C", "min": -10, "max": 50, "reference": 23, "color": "#B22222"},
     "humidity":    {"unit": "%",   "min": 20,   "max": 100, "reference": 50, "color": "#00008B"},
     "pressure":    {"unit": "hPa", "min": 850, "max": 1100, "reference": 1000, "color": "#FF4500"},
-    "co2":         {"unit": "ppm", "min": 300, "max": 1000, "reference": 420, "color": "#008080"},
+    "co2":         {"unit": "ppm", "min": 300, "max": 1000, "reference": 420, "color": "#383838"},
     "sky_temperature": {"unit": "°C", "min": -30, "max": 50, "reference": 0, "color": "#8A2BE2"},
     "rain_rate":   {"unit": "mm/h", "min": 0, "max": 30, "reference": 0, "color": "#1E90FF"},
     "wind_speed":  {"unit": "m/s", "min": 0, "max": 30, "reference": 0, "color": "#32CD32"},
