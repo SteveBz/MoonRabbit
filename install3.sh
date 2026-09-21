372d8b3d61b524b7683fa97692ea7bb618fdc6e4
Steve Cookson <steve.cookson@sca-uk.com>
2026-09-21 07:26:19 +0100

    Install MQTT broker and build tools for indi2mqtt
    Added installation commands for MQTT broker and development library, along with build tools for indi2mqtt.

--- install3.sh
+++ install3.sh
@@ -34,3 +34,17 @@ setup_config "default_sensor_value.json" "sensor_values.json" "Edit this file wi
 if [ $? -ne 2 ]; then  # Only show if at least one file was created
     echo -e "\nRemember to edit the configuration files before use!"
 fi
+
+# Install necessary packages
+sudo apt update
+sudo apt-get install -y sqlite3 sqlitebrowser
+
+# INDI
+sudo apt-get install -y indi-bin indi-aagcloudwatcher-ng
+sudo apt install  -y libindi-dev
+
+# NEW: MQTT broker and dev library (replaces pyindi-client)
+sudo apt-get install -y mosquitto mosquitto-clients libmosquitto-dev
+
+# Build tools for indi2mqtt
+sudo apt-get install -y cmake build-essential
