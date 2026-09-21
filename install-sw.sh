143a8583caf06840c2081361cea0c5f567e7f28e
Steve Cookson <steve.cookson@sca-uk.com>
2026-09-21 09:25:05 +0100

    Add indi2mqtt installation steps to install-sw.sh
    Added steps to build and install indi2mqtt from source.

--- install-sw.sh
+++ install-sw.sh
@@ -12,3 +12,19 @@ sudo apt-get install -y mosquitto mosquitto-clients libmosquitto-dev
 
 # Build tools for indi2mqtt
 sudo apt-get install -y cmake build-essential
+
+
+# NEW: build and install indi2mqtt from source
+cd /home/pi
+if [ ! -d indi2mqtt ]; then
+    git clone https://github.com/rkaczorek/indi2mqtt.git
+fi
+cd indi2mqtt
+
+# Point at the local broker (upstream defaults to a hard-coded IP)
+sed -i 's|^#define MQTT_HOST .*|#define MQTT_HOST "localhost"|' indi2mqtt.h
+
+mkdir -p build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr ..
+make
