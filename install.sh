d37f92df939fbbc4c22b017d48a9ebfa9fb32512
Steve Cookson <steve.cookson@sca-uk.com>
2026-09-21 07:25:59 +0100

    Update install.sh to remove and add packages
    Removed installation of unnecessary packages and added MQTT broker and development library.

--- install.sh
+++ install.sh
@@ -1,20 +1,6 @@
 # Sync datetime
 sudo timedatectl set-ntp true
 
-# Install necessary packages
-sudo apt update
-sudo apt-get install -y sqlite3 sqlitebrowser
-
-# INDI
-sudo apt-get install -y indi-bin indi-aagcloudwatcher-ng
-sudo apt install  -y libindi-dev
-
-# NEW: MQTT broker and dev library (replaces pyindi-client)
-sudo apt-get install -y mosquitto mosquitto-clients libmosquitto-dev
-
-# Build tools for indi2mqtt
-sudo apt-get install -y cmake build-essential
-
 # REMOVED: swig, libdbus-1-dev, pkg-config, libglib2.0-dev
 #          (these were only needed for pyindi-client)
 
